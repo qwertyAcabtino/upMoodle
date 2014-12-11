7cc30275b1a95f05086aadf48ac35d13a94255e8
Victor Perez <viperey@gmail.com>
2014-12-11 20:35:53 +0100

    Styling

--- dev/backend/backend/urls.py
+++ dev/backend/backend/urls.py
@@ -18,16 +18,20 @@ urlpatterns = patterns('rest.views',
     url(r'^admin/', include(admin.site.urls)),
 
 
-    #APIs
+    # APIs
+    # System
     url(r'^signup/$', 'signup'),
     url(r'^confirm_email/(?P<cookie>.*)/$', 'confirmEmail'),
     url(r'^login/$', 'login'),
     url(r'^logout/$', 'logout'),
     url(r'^recover_password/$', 'recoverPassword'),
+
+    # User
     url(r'^user/$', 'user'),
     url(r'^user/(?P<pk>[0-9]+)/$', 'userById'),
     url(r'^users/rol/(?P<pk>[0-9]+)/$', 'usersByRol'),
 
+    # Notes
     url(r'^note/(?P<pk>[0-9]+)/$', 'noteById'),
     url(r'^note/$', 'note')
 )
