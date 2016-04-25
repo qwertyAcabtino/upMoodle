7cdd9b75d4cf62e513630eab7b270be960461983
Viperey <viperey@gmail.com>
2016-04-25 02:50:58 +0200

    files services

--- dev/backend/rest/services/system.py
+++
@@ -1,9 +0,0 @@
-from rest.JSONResponse import JSONResponse
-from rest.models import FileType
-from rest.orm.serializers import FileTypeSerializer
-
-
-def fileTypes_get():
-    filesTypes = FileType.objects.all()
-    serializer = FileTypeSerializer(filesTypes, many=True)
-    return JSONResponse(serializer.data)
