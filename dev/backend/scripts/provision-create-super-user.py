3f16235371db8e12f971a6564cdabf0e34e08a2f
Viperey <viperey@gmail.com>
2016-05-05 18:02:12 +0200

    #21 improve deploy/provision scripts BUGFIX

--- dev/backend/scripts/provision-create-super-user.py
+++ dev/backend/scripts/provision-create-super-user.py
@@ -2,7 +2,7 @@ import pexpect
 
 child = pexpect.spawn("python manage.py createsuperuser --username admin --email viperey@gmail.com")
 child.expect('.*assword.*', timeout=4)
-child.sendline("password")
+child.sendline("12341234")
 child.expect('.*assword.*', timeout=3)
-child.sendline("password")
+child.sendline("12341234")
 child.expect(pexpect.EOF)
