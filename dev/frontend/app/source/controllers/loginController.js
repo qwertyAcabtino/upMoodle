31da8a23f8fcae80d0da25b200e149b6046cfe16
Viperey <viperey@gmail.com>
2016-05-15 15:47:36 +0200

    Login style

--- dev/frontend/app/source/controllers/loginController.js
+++ dev/frontend/app/source/controllers/loginController.js
@@ -1,6 +1,8 @@
 
 angular.module('upmApp').controller( 'loginController', ['$scope', '$cookies', 'api', 'User', '$location', 'snackbar', function($scope, $cookies, api, User, $location, snackbar){
 
+	$scope.backgroundStyle = {background: '#000'};
+	
 	$scope.sendForm = function(user){
 		var email = (user && user.email ) ? user.email : null;
 		var password = (user && user.password ) ? user.password : null;
